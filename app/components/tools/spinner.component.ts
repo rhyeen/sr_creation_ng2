7871adea079e657126990f66e53b0b33050330df
Ryan Saunders <contact.ryan.saunders@gmail.com>
2017-04-16 14:21:46 -0600

    missed a few

--- app/components/tools/spinner.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'sr-spinner',
-  templateUrl: './app/components/tools/spinner.html',
-  styleUrls: ['./app/components/tools/spinner.css']
-})
-export class SpinnerComponent {
-}
